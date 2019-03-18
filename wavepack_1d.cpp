cdb3eceba6d82f8ea0662553b08b991dd06a856c
Stephan van den Wildenberg <svdwildenberg@ulg.ac.be>
2019-03-18 22:55:44 +0100

    woups

--- wavepack_1d.cpp
+++ wavepack_1d.cpp
@@ -158,12 +158,12 @@ int main( int argc, char * argv [])
           {
              H->potential_vector(time_index,init_pot_vec);
              H->sphere_dist_read(dist_file);
-//             H->set_PICE();
+             H->set_PICE();
           }
           else
           {
              H->sphere_dist_read(dist_file);
-//             H->set_PICE();
+             H->set_PICE();
           }
           delete [] init_pot_vec;
        }
@@ -174,7 +174,7 @@ int main( int argc, char * argv [])
        {
           H->sphere_dist_read(dist_file);
           H->sphere_dist_save(dist_file);
-//          H->set_PICE();
+          H->set_PICE();
        }
         output.open(out_file.c_str());
         output<<"Output from Wavepack_1D, developped by Stephan van den Wildenberg (Theoretical Physical Chemistry, University of Liege)"<<std::endl<<"File generated on "<<date_str<<std::endl;
